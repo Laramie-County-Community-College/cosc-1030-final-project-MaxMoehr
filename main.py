bbafa0ed409e1fd015f51d9180eb977cfa82a72c
MaxMoehr <mxjones9696@gmail.com>
2025-05-10 03:51:54 +0000

    added proper function output, and made body of code and output

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ File: Make.py
 Name: Max Moehr
 
 Requirements: 
+-Monte Carlo Simulation
+-Parameters : use variables to store parameters
+-Accurately represent game logic (three point and foul scenarioes)
+-Display results : percentage of wins for each stratagy, ave number of points scored in each scenarieo
 
-Constants: 
+
+Constants: probabilities, win loss
 
 Variables: 
 #probabilities:
@@ -15,13 +20,18 @@ free_th_prob = 0.75  probability to sucsessfully score free throw
 off_reb_prob = 0.27  probability for offence rebound
 win_over_p = 0.5
 #out of trial win loss counter
-win_two_p = 0
-win_three_p = 0
-score = {win3:..., loss3:..., win2:..., loss2:...} (in progress)
+three_point_amount = how many games won by three strat
+two_point_amount = how many games won by two point foul strat
+score_saver_two = where score for each two point match are kept
+score_saver_three = score for three point match is stored
+win_three_p = how many times three point won
+win_two_p = how many times two point won
 #in trial score counter
 team = 0
 opp_team = 3
 timer = 30
+loss = 0
+win = 1
 
 Output: average score and percentage of wins for each strat
 
@@ -30,10 +40,23 @@ addition
 average
 
 Key design considerations: 
-Returning average as output
+Returning average and percentage as output
 
 
 Test data: 
+output: 'enter number of trials:'
+user input: 200
+output:
+When in the last thirty seconds of a game, three points down it was found:
+ The percentage chance of winning with a three point stratagy: 
+  16.0%
+ The percentage chance of winning with a two point and foul stratagy:  
+  23.5%
+ The average points scored using a three point stratagy: 
+  1.9
+ The average points scored using a two point and foul stratagy: 
+  8.4
+
 '''
 import random
 
@@ -45,6 +68,8 @@ def two_point_choice(): #two point strat function
     opp_team = 3
     timer = 30
     off_reb_prob = 0.27
+    win = 1
+    loss = 0
 
     out_come_two_p = random.random() #random number 
     timer -= 1 #time to take shot
@@ -60,24 +85,29 @@ def two_point_choice(): #two point strat function
         while timer > 0: #countdown continues after free throw is made (fixed?)
             off_reb = random.random() #rebound random number generator
             if off_reb >= off_reb_prob: # decides if we get ball or opponent gets ball
-                shot_attempt = random.random()
+                shot_attempt = random.random() #if our team misses or not
                 if shot_attempt >= two_p_prob:
                     team += 2
                     timer -= 3
             else:
-                shot_attempt_off = random.random()
+                shot_attempt_off = random.random() #weather or not opposing team misses
                 if shot_attempt_off >= two_p_prob:
                     opp_team += 2
                     timer -= 3
-    if team > opp_team: #bandaid for win counter
+    
+    if team > opp_team: # sends wins and losses and score to body
         win = 1 
-        return win
+        return win, team
+    
     elif team < opp_team:
         loss = 0
-        return loss
-
-#^make return certain value to main body so response can be sorted into dict to be calculated
-
+        return loss, team
+    else:
+        overtime = random.random()
+        if overtime > 0.5:
+            return win, team
+        else:
+            return loss, team
 
 
 
@@ -87,6 +117,8 @@ def three_point_choice(): #three point stratagy trial function
     timer = 30
     three_p_prob = 0.33
     win_over_p = 0.5
+    win = 1
+    loss = 0
 
     out_come_three_p = random.random() # random number for getting a 3 point shot
     timer -= 1
@@ -95,29 +127,56 @@ def three_point_choice(): #three point stratagy trial function
         win_over = random.random() # random number to say if team won over time (50/50)
         if win_over >= win_over_p: # if random number larger than 0.5 then win is counted
             win = 1
-            return win
+            return win, team
         else:
             loss = 0
-            return loss
+            return loss, team
+    else:
+        return loss, team
      
-#same with this one
+#asks user for trial number
+num_trials = int(input("enter number of trials: "))
 
-def basket_ball_sim(num_trials): # where average and percentage is calculated 
-    win_two_p = 0 # proto win counter
-    win_three_p = 0
-    for _ in range(num_trials): # tells it to iterate over num_trials
-        choice = random.random() # random generator decides what strat is choosen
-        if choice >= 0.5: #50/50 chance of 3 pt or 2 pt 
-            three_point_out = three_point_choice() # where 3 point trials are documented
-            if three_point_out == 1:
-                win_three_p += 1
-        else:
-            two_point_out = two_point_choice() # where 2 point trials are documented
-            if two_point_out == 1:
-                win_two_p += 1
-    percent_three_p = (win_three_p/num_trials) * 100 # percent calculation 
-    percent_two_p = (win_two_p/num_trials) * 100
-    print(percent_three_p,percent_two_p)
-    
-        
-basket_ball_sim(100) # proto base code 
+#variables, lists
+three_point_amount = 0
+two_point_amount = 0
+score_saver_two = []
+score_saver_three = []
+win_three_p = 0
+win_two_p = 0
+
+for _ in range(num_trials): # tells it to iterate over num_trials
+    choice = random.random() # random generator decides what strat is choosen
+    if choice >= 0.5: #50/50 chance of 3 pt or 2 pt 
+        three_point_out, score_three = three_point_choice() #unpacking win or loss and score
+        three_point_amount += 1 #counts how many times three strat was used
+        if score_three != 0:
+            score_saver_three.append(score_three) #removes zeros
+        if three_point_out == 1:
+                win_three_p += 1 #counts how many times three point strat won
+    else:
+        two_point_out, score_two = two_point_choice() # where 2 point trials are documented
+        two_point_amount += 1 #count of two point foul strat games
+        if score_two != 0: #removes zeros
+            score_saver_two.append(score_two)
+        if two_point_out == 1:
+            win_two_p += 1 #counts how many times two point foul strat won
+
+
+
+ #sums score lists
+sum_three = sum(score_saver_three)
+sum_two = sum(score_saver_two)
+ #averages score to points per game
+ave_score_two = sum_two / two_point_amount
+ave_score_three = sum_three / three_point_amount
+ #percent calculation for each strat
+percent_three_p = (win_three_p/num_trials) * 100 
+percent_two_p = (win_two_p/num_trials) * 100
+
+ #gives to user as output
+print(f'When in the last thirty seconds of a game, three points down it was found:')
+print(f' The percentage chance of winning with a three point stratagy: \n  {percent_three_p:.1f}%')
+print(f' The percentage chance of winning with a two point and foul stratagy:  \n  {percent_two_p:.1f}%')
+print(f' The average points scored using a three point stratagy: \n  {ave_score_three:.1f}')
+print(f' The average points scored using a two point and foul stratagy: \n  {ave_score_two:.1f}')
